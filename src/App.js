baf19ee0cdcd896f289a7173a5f61cefcf1f8de2
Chrystiansantos <chrystian_santos_silva@hotmail.com>
2020-03-04 22:30:28 -0300

    Configurando o redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+
+import store from './store';
 
 function App() {
   /* Irei importar o BrowserRouter aqui pois assim o header ira consegui acessar
   as rotas */
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
