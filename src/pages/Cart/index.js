09a5c7c02b70534a1a5c8335eb8bdcf5c261959e
Chrystiansantos <chrystian_santos_silva@hotmail.com>
2020-03-08 23:38:02 -0300

    Calculando totais

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -7,10 +7,11 @@ import {
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as CartActions from '../../store/modules/cart/actions';
+import { formatPrice } from '../../util/format';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
   function increment(product) {
     updateAmount(product.id, product.amount + 1);
   }
@@ -52,7 +53,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 </div>
               </td>
               <td>
-                <strong>R$ 258,80</strong>
+                <strong>{product.subTotal}</strong>
               </td>
               <td>
                 <button
@@ -72,7 +73,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
         <Total>
           <span>
             TOTAL
-            <strong>R$ 1920,90</strong>
+            <strong>{total}</strong>
           </span>
         </Total>
       </footer>
@@ -80,7 +81,15 @@ function Cart({ cart, removeFromCart, updateAmount }) {
   );
 }
 const mapStateToProps = state => ({
-  cart: state.cart,
+  cart: state.cart.map(product => ({
+    ...product,
+    subTotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
 });
 
 const mapDispatchToProps = dispatch =>
